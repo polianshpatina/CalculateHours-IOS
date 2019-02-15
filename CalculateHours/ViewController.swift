0636e90f299a94c4f6a3940be922e96237ef58ee
ccc <ccc@UFTs-MacBook-Pro.local>
2019-02-15 14:35:08 +0100

    Initial Commit

---
+++ CalculateHours/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CalculateHours
+//
+//  Created by ccc on 15/02/2019.
+//  Copyright © 2019 UFT. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
